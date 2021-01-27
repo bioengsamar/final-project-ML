06d04d0b33baa3c8d905b73f9ff225a984541964
unknown <ibrahimsamar858@gmail.com>
2021-01-27 23:47:34 +0200

    modified knn

--- Knn.py
+++ Knn.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     x, y=load_data(path)
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1,random_state=42)
     accuracy= model(X_train, X_test, y_train, y_test)
-    print('Model accuracy is: ', accuracy) #0.8873239436619719 with and without center data & without normalization
+    print('Model accuracy is: ', accuracy) #0.8873239436619719 with and without center data & without normalization and scaling
                                            #0.92018779342723 with center data, normalization and scaling
                                            
